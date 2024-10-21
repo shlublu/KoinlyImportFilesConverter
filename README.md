b17394b039c13ee631d1a9df2c585613e97634fa
shlublu <shlublu@yahoo.fr>
2024-10-21 18:04:23 +0200

    Etherlink token support

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # KoinlyImportFilesConverter
-Converts history files from various CEX to Koinly import files.
+- Converts history files from various CEX to Koinly import files.
+- Displays the balance changes engendered by these generated Koinly import files.
 
 ## Usage
-`koinly_convert.py meria|etherlink path/to/file.csv`
+- `koinly_convert.py meria|etherlink path/to/file.csv [path/to/etherlink_tokens_transfer_file.csv (Etherlink only)]`
+    - Meria input files should be generated as 'WaltioCSV' files from https://www.meria.com 
+    - Etherlink input files should be generated from https://explorer.etherlink.com
+
+- `koinly_check.py path/to/file.csv`
+    - The input file should be a Koinly import file generated with `koinly_convert.py`
 
 ## Disclaimer
 I built this tool for my own use, and I apologize as it looks a bit quick'n'dirty. 
@@ -15,4 +21,4 @@ The default fiat currency is EUR. Please adjust the variable 'FIAT_BASE_CURRENCY
 EUPL 1.2 https://joinup.ec.europa.eu/sites/default/files/custom-page/attachment/2020-03/EUPL-1.2%20EN.txt
 
 ## Author
-Vincent Poulain, December 2022
+Vincent Poulain, 2022-2024
--- koinly_convert.py
+++ koinly_convert.py
@@ -1,9 +1,9 @@
 #!/bin/python3
 
 '''
-Koinly Converter: converts history files from various CEX (Meria, Etherlink) to Koinly import files.
+Koinly Convert: converts history files from various CEX (to date: Meria, Etherlink) to Koinly import files.
 
-Usage: koinly_convert.py meria path/to/file.csv
+Usage: koinly_convert.py meria|etherlink_xtz|etherlink_tokens path/to/file.csv
 
 Disclaimer: I built this tool for my own use, and I apologize as it looks a bit quick'n'dirty. 
             I am sharing it because if it was useful to me, it might be useful to others. However, it comes with no guarantee of any kind.
@@ -12,14 +12,17 @@ Default fiat currency notice: the default fiat currency is EUR. Please adjust th
                               This is very important for Binance Card export files as this corresponds to the card's currency. This is expected to be minor for export files from other CEXs.
 
 Licence: EUPL 1.2 https://joinup.ec.europa.eu/sites/default/files/custom-page/attachment/2020-03/EUPL-1.2%20EN.txt
-Author: Vincent Poulain, December 2022
+Author: Vincent Poulain, 2022-2024
 '''
 
 from __future__ import annotations
 
 import csv
+import logging
 import os
 import sys
+
+from typing import TextIO
 
 
 FIAT_BASE_CURRENCY = 'EUR'
@@ -27,16 +30,22 @@ FIAT_BASE_CURRENCY = 'EUR'
 CSV_DELIMITER_OUT = ';'
 CSV_DELIMITER_IN_BINANCECARD = ';'
 
+MODE_MERIA = 'meria'
+MODE_ETHERLINK = 'etherlink'
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 class OutputLine:
     def __init__(
-        self, 
-        txDate: str, 
-        sentAmount: str, sentCurrency: str, 
-        receivedAmount: str, receivedCurrency: str, *, 
-        feeAmount: str = None, feeCurrency: str = None, 
-        netWorthAmount: str = None, netWorthCurrency: str = None, 
-        label: str = None, description: str = None, txHash: str = None
+            self, 
+            txDate: str, 
+            sentAmount: str, sentCurrency: str, 
+            receivedAmount: str, receivedCurrency: str, *, 
+            feeAmount: str = None, feeCurrency: str = None, 
+            netWorthAmount: str = None, netWorthCurrency: str = None, 
+            label: str = None, description: str = None, txHash: str = None
         ) -> None:
         self.txDate = txDate
         self.sentAmount = sentAmount
@@ -63,7 +72,11 @@ class OutputLine:
             self.description,
             self.txHash
         ]
+    
 
+    def __repr__(self) -> str:
+        return repr(self.toList())
+    
 
     @staticmethod
     def headers() -> OutputLine:
@@ -79,41 +92,47 @@ class OutputLine:
         )
 
 
+def usage() -> None:
+    logger.error(f'Usage: {sys.argv[0]} {MODE_MERIA}|{MODE_ETHERLINK} path/to/transaction_file.csv [path/to/etherlink_tokens_transfer_file.csv]')
+
+
 def doConvert() -> None:
-    MODE_MERIA = 'meria'
-    MODE_ETHERLINK = 'etherlink'
+    if len(sys.argv) in (3, 4):
+        mode = sys.argv[1]
 
-    if len(sys.argv) != 3:
-        print(
-            f'Usage: {sys.argv[0]} {MODE_MERIA}|{MODE_ETHERLINK} path/to/file.csv', 
-            file = sys.stderr
-        )
-
+        if (len(sys.argv) == 4 and mode == MODE_MERIA) or (len(sys.argv) == 3 and mode == MODE_ETHERLINK):
+            return usage()
     else:
-        filePath = sys.argv[2]
+        return usage()
+    
+    filePathA = sys.argv[2]
+    filePathB = None if mode != MODE_ETHERLINK else sys.argv[3]
 
-        try:
-            with open(filePath, newline = '') as inputFile:
-                mode = sys.argv[1]
-                lines = None
+    lines = None
 
-                if mode == MODE_MERIA:
-                    lines = convertMeria(inputFile)
-                elif mode == MODE_ETHERLINK:
-                    lines = convertEtherlink(inputFile)
-                else:
-                    print(f'Unknown mode: {mode}. Try "{sys.argv[0]} help" for help.', file = sys.stderr)
+    try:
+        with open(filePathA, newline = '') as inputFileA:
+            if mode == MODE_MERIA:
+                lines = convertMeria(inputFileA)
 
-        except FileNotFoundError:
-            print(f'Cannot open "{filePath}": file not found.', file = sys.stderr)
+            elif mode == MODE_ETHERLINK:
+                with open(filePathB, newline = '') as inputFileB:
+                    lines = consolidateEtherlink(sorted(convertEtherlinkXtz(inputFileA) + convertEtherlinkTokens(inputFileB), key = lambda x: x.txDate))            
 
-        splittedPath = os.path.split(filePath)
+            else:
+                logger.error(f'Unknown mode: {mode}. Try "{sys.argv[0]} help" for help.')
+
+        splittedPath = os.path.split(filePathA)
 
         with open(os.path.join(splittedPath[0], f'koinly_{splittedPath[1]}'), mode = 'w', newline = '') as outputFile:
             writer = csv.writer(outputFile, delimiter = CSV_DELIMITER_OUT, quotechar='"', quoting=csv.QUOTE_MINIMAL)
             writer.writerow(OutputLine.headers().toList())
+
             for row in lines:
                 writer.writerow(row.toList())
+
+    except FileNotFoundError as err:
+        logger.error(f'Cannot open "{err.filename}": file not found.')
 
 
 def csvReader(inputFile: str, delimiter: str) -> csv.reader:
@@ -123,9 +142,28 @@ def csvReader(inputFile: str, delimiter: str) -> csv.reader:
     return reader
 
 
-def convertMeria(inputFile: str) -> list[OutputLine]:
+def toUnits(amount: str, decimals: str) -> str:
+    intDecimals = int(decimals)
+    floatResult = int(amount) / int(f'1{intDecimals * "0"}')
+
+    strResult = f'{floatResult:.{intDecimals}f}'
+
+    while (strResult[-1] == '0'):
+        strResult = strResult[:-1]
+  
+    if strResult[-1] == '.':
+        strResult = strResult[:-1]
+        
+    return strResult
+
+
+def sameAmount(amountA: str, amountB: str):
+    return abs(float(amountA) - float(amountB)) <= 3e-6
+    
+
+def convertMeria(inputFile: TextIO) -> list[OutputLine]:
     def unhandledTxInfoForTxTypeError(txType: str, txInfo: str):
-        print(f'Unhandled txInfo for txType {txType}: {txInfo}.', file = sys.stderr)
+        logger.error(f'Unhandled txInfo for txType {txType}: {txInfo}.')
 
     normalizeLunaTicker = lambda ticker : ticker if ticker != 'LUNA' else f'{ticker}2'
 
@@ -231,7 +269,7 @@ def convertMeria(inputFile: str) -> list[OutputLine]:
                 unhandledTxInfoForTxTypeError(txType, txInfo)
 
         else:
-            print(f'Unhandled txType: {txType}.', file = sys.stderr)
+            logger.error(f'Unhandled txType: {txType}.')
 
         lines.append(
             OutputLine(
@@ -248,12 +286,12 @@ def convertMeria(inputFile: str) -> list[OutputLine]:
     return lines
 
 
-def convertEtherlink(inputFile: str) -> list[OutputLine]:
+def convertEtherlinkXtz(inputFile: TextIO) -> list[OutputLine]:
     reader = csvReader(inputFile, ',')
     lines = []
 
     def toXtz(amount: str) -> str:
-        return str(int(amount) / 1000000000000000000)
+        return toUnits(amount, 18)
 
     for row in reader:
         txHash = row[0]
@@ -288,7 +326,7 @@ def convertEtherlink(inputFile: str) -> list[OutputLine]:
             label = None
 
         else:
-            print(f'Unhandled txType: {txType}.', file = sys.stderr)
+            logger.error(f'Unhandled txType: {txType}.')
 
         description = f'{txType}{(" (" + methodName + ")") if len(methodName) > 0 else ""}: {fromAddress} to {toAddress}' 
 
@@ -305,6 +343,275 @@ def convertEtherlink(inputFile: str) -> list[OutputLine]:
         )
 
     return lines
+
+
+def convertEtherlinkTokens(inputFile: TextIO) -> list[OutputLine]:
+    reader = csvReader(inputFile, ',')
+    lines = []
+    
+    for row in reader:
+        txHash = row[0]
+        txDate = row[2]
+        fromAddress = row[3]
+        toAddress = row[4]      
+        contractAddress = row[5]  
+        txType = row[6]
+        tokenDecimals = row[7]
+        tokenSymbol = row[8]
+        amount = row[9]
+
+        sentAmount = None
+        sentCurrency = None
+        receivedAmount = None
+        receivedCurrency = None
+        feeAmount = None
+        feeCurrency = None
+        label = None
+        description = None        
+
+        if len(tokenDecimals) == 0:
+            tokenDecimals = 0
+
+        if txType == 'IN':
+            receivedAmount = toUnits(amount, tokenDecimals)
+            receivedCurrency = tokenSymbol
+            label = None
+
+        elif txType == 'OUT':
+            sentAmount = toUnits(amount, tokenDecimals)
+            sentCurrency = tokenSymbol
+
+            if sentCurrency[:3]  == 'slW' and  toAddress == '0x65fe928c5D04a2DA42347bA9D4d1C3f4952851F5' and contractAddress == '0x008ae222661B6A42e3A097bd7AAC15412829106b':
+                receivedAmount = sentAmount
+                receivedCurrency = sentCurrency[3:]
+                label = 'swap'
+                description = f'Unwrapped {sentAmount} {sentCurrency} to {receivedAmount} {receivedCurrency}'
+
+            else:           
+                label = None
+
+        else:
+            logger.error(f'Unhandled txType: {txType}.')
+
+        if label is None:
+            description = f'{txType}: {fromAddress} to {toAddress}' 
+
+        lines.append(
+            OutputLine(
+                txDate = txDate,
+                sentAmount = sentAmount, sentCurrency = sentCurrency,
+                receivedAmount = receivedAmount, receivedCurrency = receivedCurrency,
+                feeAmount = feeAmount, feeCurrency = feeCurrency,
+                label = label,
+                description = description,
+                txHash = txHash
+            )
+        )
+
+    return lines
+
+
+def consolidateEtherlink(txList: list[OutputLine]) -> list[OutputLine]:
+    def getTxByIndex(txList: list[OutputLine], index: int) -> OutputLine:
+        try:
+            return txList[index]
+        
+        except IndexError:
+            return OutputLine(None, None, None, None, None)
+        
+
+    consolidatedTxs = []
+    skipNext = 0
+
+    for idx in range(len(txList)):
+        tx = txList[idx]
+
+        if skipNext > 0:
+            skipNext -= 1
+
+        elif tx.description.startswith('OUT (depositETH):'):
+            txBack = getTxByIndex(txList, idx + 1)
+
+            if (
+                txBack.txDate != tx.txDate or 
+                txBack.sentAmount is not None or 
+                txBack.sentCurrency is not None or 
+                not sameAmount(txBack.receivedAmount, tx.sentAmount) or 
+                txBack.receivedCurrency != f'slW{tx.sentCurrency}' or
+                txBack.txHash != tx.txHash
+            ):
+                logger.error(f'No consistent back transaction for OUT depositETH: {tx.txDate}')
+                consolidatedTxs.append(tx)
+
+            else:
+                consolidatedTxs.append(OutputLine(
+                        txDate = tx.txDate, 
+                        sentAmount = tx.sentAmount, sentCurrency = tx.sentCurrency, 
+                        receivedAmount = txBack.receivedAmount, receivedCurrency = txBack.receivedCurrency,
+                        feeAmount = tx.feeAmount, feeCurrency = tx.feeCurrency, 
+                        label = 'swap', description = f'Deposited {tx.sentAmount} {tx.sentCurrency}',
+                        txHash = tx.txHash
+                    )
+                )
+
+                skipNext = 1
+
+        elif tx.description.startswith('OUT (supply):'):
+            txBackA = getTxByIndex(txList, idx + 1)
+            txBackB = getTxByIndex(txList, idx + 2)
+
+            if (
+                txBackA.txDate != tx.txDate or txBackB.txDate != tx.txDate or
+                txBackA.sentAmount is None or 
+                txBackA.sentCurrency is None or 
+                not sameAmount(txBackB.receivedAmount, txBackA.sentAmount) or 
+                txBackB.receivedCurrency != f'sl{txBackA.sentCurrency}' or
+                txBackA.txHash != tx.txHash or txBackB.txHash != tx.txHash
+            ):
+                logger.error(f'No consistent back transactions for supply: {tx.txDate}')
+                consolidatedTxs.append(tx)
+
+            else:
+                tx.description = f'Unlocked {txBackA.sentCurrency} for OUT supply'
+                consolidatedTxs.append(tx)
+
+                consolidatedTxs.append(OutputLine(
+                        txDate = tx.txDate, 
+                        sentAmount = txBackA.sentAmount, sentCurrency = txBackA.sentCurrency, 
+                        receivedAmount = txBackB.receivedAmount, receivedCurrency = txBackB.receivedCurrency,
+                        feeAmount = txBackA.feeAmount, feeCurrency = tx.feeCurrency, 
+                        label = 'swap', description = f'Supplied {txBackA.sentAmount} {txBackA.sentCurrency}',
+                        txHash = tx.txHash
+                    )
+                )
+
+                skipNext = 2
+
+        elif tx.description.startswith('OUT (withdrawETH):'):
+            txBack = getTxByIndex(txList, idx + 1)
+
+            if (
+                txBack.txDate != tx.txDate or 
+                txBack.sentAmount is not None or 
+                txBack.sentCurrency is not None or 
+                txBack.receivedAmount is None or 
+                txBack.receivedCurrency != f'slW{tx.sentCurrency}' or
+                txBack.txHash != tx.txHash
+            ):
+                logger.error(f'No consistent back transaction for OUT withdrawETH: {tx.txDate}')
+                consolidatedTxs.append(tx)
+
+            else:
+                consolidatedTxs.append(OutputLine(
+                        txDate = tx.txDate, 
+                        sentAmount = tx.sentAmount, sentCurrency = tx.sentCurrency, 
+                        receivedAmount = txBack.receivedAmount, receivedCurrency = txBack.receivedCurrency,
+                        feeAmount = tx.feeAmount, feeCurrency = tx.feeCurrency, 
+                        label = None, description = f'Unlocked {tx.sentCurrency} for redeem',
+                        txHash = tx.txHash
+                    )
+                )
+
+                skipNext = 1
+
+        elif tx.description.startswith('OUT (withdraw):'):
+            txBackA = getTxByIndex(txList, idx + 1)
+            txBackB = getTxByIndex(txList, idx + 2)
+
+            if (
+                txBackA.txDate != tx.txDate or txBackB.txDate != tx.txDate or
+                txBackA.sentAmount is None or 
+                txBackA.sentCurrency != f'sl{txBackB.receivedCurrency}' or 
+                not sameAmount(txBackB.receivedAmount, txBackA.sentAmount) or 
+                txBackB.receivedCurrency is None or
+                txBackA.txHash != tx.txHash or txBackB.txHash != tx.txHash
+            ):
+                logger.error(f'No consistent back transactions for withdraw: {tx.txDate}')
+                consolidatedTxs.append(tx)
+
+            else:
+                tx.description = f'Unlocked {txBackA.sentCurrency} for OUT withdraw'
+                consolidatedTxs.append(tx)
+
+                consolidatedTxs.append(OutputLine(
+                        txDate = tx.txDate, 
+                        sentAmount = txBackA.sentAmount, sentCurrency = txBackA.sentCurrency, 
+                        receivedAmount = txBackB.receivedAmount, receivedCurrency = txBackB.receivedCurrency,
+                        feeAmount = txBackA.feeAmount, feeCurrency = tx.feeCurrency, 
+                        label = 'swap', description = f'Redeemed {txBackA.sentAmount} {txBackA.sentCurrency}',
+                        txHash = tx.txHash
+                    )
+                )
+
+                skipNext = 2
+                
+        elif tx.description.startswith('OUT (multicall):'):
+            txBack = getTxByIndex(txList, idx + 1)
+
+            if (
+                txBack.txDate != tx.txDate or 
+                txBack.sentAmount is not None or 
+                txBack.sentCurrency is not None or 
+                txBack.receivedAmount is None or 
+                txBack.receivedCurrency == tx.receivedCurrency or
+                txBack.txHash != tx.txHash
+            ):
+                logger.error(f'No consistent back transaction for OUT multicall: {tx.txDate}')
+                consolidatedTxs.append(tx)
+
+            else:
+                consolidatedTxs.append(OutputLine(
+                        txDate = tx.txDate, 
+                        sentAmount = tx.sentAmount, sentCurrency = tx.sentCurrency, 
+                        receivedAmount = txBack.receivedAmount, receivedCurrency = txBack.receivedCurrency,
+                        feeAmount = tx.feeAmount, feeCurrency = tx.feeCurrency, 
+                        label = 'swap', description = f'Swapped {tx.sentAmount} {tx.sentCurrency} to {txBack.receivedAmount} {txBack.receivedCurrency}',
+                        txHash = tx.txHash
+                    )
+                )
+
+                skipNext = 1
+
+        elif tx.description.startswith('OUT (bridge):'):
+            txBack = getTxByIndex(txList, idx + 1)
+
+            if (
+                txBack.txDate != tx.txDate or
+                tx.sentAmount is None or
+                tx.sentCurrency != 'XTZ' or
+                txBack.sentAmount is None or
+                txBack.sentCurrency is None or
+                txBack.txHash != tx.txHash
+            ):
+                logger.error(f'No consistent back transaction for OUT bridge: {tx.txDate}')
+                consolidatedTxs.append(tx)
+
+            else:
+                consolidatedTxs.append(OutputLine(
+                        txDate = tx.txDate, 
+                        sentAmount = None, sentCurrency = None,
+                        receivedAmount = None, receivedCurrency = None,
+                        feeAmount = tx.sentAmount, feeCurrency = tx.sentCurrency, 
+                        label = None, description = f'Bridge foreign gas fees',
+                        txHash = tx.txHash
+                    )
+                )
+
+                consolidatedTxs.append(OutputLine(
+                        txDate = tx.txDate, 
+                        sentAmount = txBack.sentAmount, sentCurrency = txBack.sentCurrency,
+                        receivedAmount = None, receivedCurrency = None,
+                        label = None, description = f'Bridged out {txBack.sentAmount} {txBack.sentCurrency}',
+                        txHash = tx.txHash
+                    )
+                )
+
+                skipNext = 1
+
+        else:
+            consolidatedTxs.append(tx)
+
+    return consolidatedTxs
 
 
 if __name__ == '__main__':
